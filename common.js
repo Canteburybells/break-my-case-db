f9227eb478d3bae4208e77fe889e821bb7d58844
Canteburybells <clarkia.0523@gmail.com>
2026-07-06 01:19:56 +0900

    add

--- common.js
+++ common.js
@@ -102,8 +102,7 @@ const eventOrder = [
   "2nd Anniversary -Dualism-",
   "Place your bets 裏返しの機略",
   "Cherish 日々を歩む息吹",
-  "Vignette -Emperor/Skip-",
-  "Milky Way 幾重の想いに約束を"
+  "Vignette -Emperor/Skip-"
 ]
 
 const characterOrder = [
